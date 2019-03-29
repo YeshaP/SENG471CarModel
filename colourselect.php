01941a3f44bb3f03d790ee71741ea51da145e60e
nicole-ec <nicole.eneco@yahoo.ca>
2019-03-29 17:17:24 -0600

    new model files

--- colourselect.php
+++ colourselect.php
@@ -4,30 +4,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <script src="changepics.js" type="text/javascript"></script>
-    <link href="test.css" rel="stylesheet" type="text/css">
+    <link href="format.css" rel="stylesheet" type="text/css">
   </head>
+<body>
+  <h1 class="heading"> Customize your Model </h1>
+  <hr>
 
-<div class="row">
-  <div class="column">
-    <img src="https://media.wired.com/photos/59bafdf204afdc5248726f5c/master/w_2400,c_limit/BMW-TA.jpg" class="exterior" alt="exterior">
+  <div class="row">
+    <div class="column">
+      <img src="https://media.wired.com/photos/59bafdf204afdc5248726f5c/master/w_2400,c_limit/BMW-TA.jpg" class="exterior" alt="exterior">
+    </div>
+    <div class="column">
+      <div class="colourbutton">
+        <button onclick="changePics()">
+           <img src="https://hoseco.com.au/chwp/wp-content/uploads/2018/03/Light-Blue-Box.jpg"
+             alt="blue" class="colour"/>
+        </button><br><br>
+        <button onclick="changePics()">
+           <img src="https://hoseco.com.au/chwp/wp-content/uploads/2018/03/Light-Blue-Box.jpg"
+             alt="blue" class="colour"/>
+        </button><br><br>
+        <button onclick="changePics()">
+           <img src="https://hoseco.com.au/chwp/wp-content/uploads/2018/03/Light-Blue-Box.jpg"
+             alt="blue" class="colour"/>
+        </button>
+      </div>
+    </div>
   </div>
-  <div class="column">
-    <img src="https://s3.amazonaws.com/external_clips/attachments/1413701/original/interior.png?1504612523" class="interior" alt="interior">
-  </div>
-</div>
 
-<div class="row">
-  <div class="column">
-    <button onclick="changePics()">
-      <img src="https://hoseco.com.au/chwp/wp-content/uploads/2018/03/Light-Blue-Box.jpg"
-        alt="blue" class="colourbutton"/>
-    </button>
+  <div id="innerdiv">
   </div>
-</div>
-
+</body>
 
 <?php
 require_once "connectdb.php";
 // <h1 class="heading">Test</h1>
+
+// <button onclick="changePics()">
+//   <img src="https://hoseco.com.au/chwp/wp-content/uploads/2018/03/Light-Blue-Box.jpg"
+//     alt="blue" class="colourbutton"/>
+// </button>
+
+// <img src="https://s3.amazonaws.com/external_clips/attachments/1413701/original/interior.png?1504612523" class="interior" alt="interior">
+
+// .column img {
+//   margin-top: 8px;
+//   vertical-align: middle;
+// }
 ?>
 </html>
